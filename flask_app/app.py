31aa24f43516fe912c1b5206eaf1a5689cab6729
bissakov <78418192+bissakov@users.noreply.github.com>
2023-04-09 23:20:37 +0600

    Load test json file

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request, render_template
 from family_data.family import get_family_data
 
@@ -6,14 +7,14 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    result = None
+def home() -> str:
+    family = None
     data = request.form.get('data', '')
     if request.method == 'POST':
-        iin = request.form['data']
         # result = get_family_data(iin)
-        result = iin
-    return render_template('index.html', data=data, result=result)
+        with open(r'D:\Work\python_rpa\arta_profiles\family_data\data.json', 'r', encoding='utf-8') as f:
+            family = json.load(f)
+    return render_template('index.html', data=data, family=family)
 
 
 if __name__ == '__main__':
