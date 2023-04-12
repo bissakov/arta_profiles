8fc9d9cc9b8fdf6af72f44a81901ec1d8e48ba7d
bissakov <78418192+bissakov@users.noreply.github.com>
2023-04-12 11:30:26 +0600

    to_dict method for getting dictionary for website

--- family_data/entities.py
+++ family_data/entities.py
@@ -31,9 +31,16 @@ class Risks:
     school: str = None
 
     def to_dict(self):
-        return {risk.name: getattr(self, risk.name)
-                for risk in fields(self)
-                if getattr(self, risk.name)}
+        names = [
+            'Уровень дохода ниже ЧБ',
+            'Семья имеет задолженность по кредиту больше 90 дней',
+            'Член семьи не имеет прикрепление к медицинской организации',
+            'Член семьи состоит на диспансерном учете',
+            'Член семьи не имеет обязательное социальное медицинское страхование',
+            'Дети не посещают дошкольные организации',
+            'Дети не посещают школы'
+        ]
+        return [name for risk, name in zip(fields(self), names) if getattr(self, risk.name)]
 
 
 @dataclass
@@ -43,6 +50,17 @@ class Recommendations:
     need_med: bool = False
     need_emp: bool = False
     need_nedv: bool = False
+
+    def to_dict(self) -> List[str]:
+        names = ['АСП', 'Образование', 'Медицина', 'Трудоустройство', 'Жилье']
+        return [name for recommendation, name in zip(fields(self), names) if getattr(self, recommendation.name)]
+
+
+@dataclass
+class Assets:
+    land_cnt: int = 0
+    emp_cnt: int = 0
+    soc_pay_recipient_cnt: int = 0
 
     def to_dict(self) -> Dict[str, bool]:
         return {recommendation.name: getattr(self, recommendation.name)
@@ -64,29 +82,27 @@ class Family:
     per_capita_income_asp: int = 0
     income: str = None
     recommendations: Recommendations = Recommendations()
-    land_cnt: int = 0
-    emp_cnt: int = 0
-    soc_pay_recipient_cnt: int = 0
+    assets: Assets = Assets()
     risks: Risks = Risks()
     social_status: Dict[str, int] = field(default_factory=get_social_status_dict)
 
     def to_dict(self) -> Dict:
         return {
             'members': [member.full_name for member in self.members],
-            'member_cnt': self.member_cnt,
-            'child_cnt': self.child_cnt,
-            'family_level': self.family_level,
-            'address': self.address,
-            'salary': self.salary,
-            'social_payment': self.social_payment,
-            'per_capita_income': self.per_capita_income,
-            'total_income_asp': self.total_income_asp,
-            'per_capita_income_asp': self.per_capita_income_asp,
-            'income': self.income,
+            'member_cnt': {'name': 'Кол-во человек', 'value': self.member_cnt},
+            'child_cnt': {'name': 'Кол-во детей', 'value': self.child_cnt},
+            'family_level': {'name': 'Уровень семьи', 'value': self.family_level},
+            'address': {'name': 'Адрес', 'value': self.address},
+            'salary': {'name': 'Зарплата', 'value': self.salary},
+            'social_payment': {'name': 'Социальные выплаты', 'value': self.social_payment},
+            'per_capita_income': {'name': 'Среднедушевой доход', 'value': self.per_capita_income},
+            'total_income_asp': {'name': 'Совокупный доход для АСП', 'value': self.total_income_asp},
+            'per_capita_income_asp': {'name': 'Среднедушевой доход АСП', 'value': self.per_capita_income_asp},
+            'income': {'name': 'Доход', 'value': self.income},
             'recommendations': self.recommendations.to_dict(),
-            'land_cnt': self.land_cnt,
-            'emp_cnt': self.emp_cnt,
-            'soc_pay_recipient_cnt': self.soc_pay_recipient_cnt,
+            'land_cnt': {'name': 'Жилая недвижимость', 'value': self.assets.land_cnt},
+            'emp_cnt': {'name': 'Кол-во трудоустроенных членов семьи', 'value': self.assets.emp_cnt},
+            'soc_pay_recipient_cnt': {'name': 'Получатели социальных выплат', 'value': self.assets.soc_pay_recipient_cnt},
             'risks': self.risks.to_dict(),
             'social_status': {key: value for key, value in self.social_status.items() if value > 0},
         }
