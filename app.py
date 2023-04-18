e80ea498a918c8fdfdf6dfc65ea2eeae65b3f752
bissakov <78418192+bissakov@users.noreply.github.com>
2023-04-18 10:25:42 +0600

    guard clause

--- app.py
+++ app.py
@@ -11,20 +11,24 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home() -> str:
+    iin = request.form.get('data', '')
+
+    if request.method != 'POST':
+        return render_template('index.html', data=iin, family=None, error=None)
+
     family = None
     error_msg = None
-    iin = request.form.get('data', '')
-    if request.method == 'POST':
-        try:
-            family = get_family_data(iin)
-        except FamilyNotFound:
-            error_msg = 'Неправильный ИИН'
-        except httpx.HTTPError:
-            error_msg = 'Connection error'
-        except WrongPassword:
-            error_msg = 'Неправильный пароль. Свяжитесь с администраторами'
-        except FamilyNotInList:
-            error_msg = 'ИИН не найден в списке "Потенциальные получатели АСП с детьми"'
+
+    try:
+        family = get_family_data(iin)
+    except FamilyNotFound:
+        error_msg = 'Неправильный ИИН'
+    except httpx.HTTPError:
+        error_msg = 'Connection error'
+    except WrongPassword:
+        error_msg = 'Неправильный пароль. Свяжитесь с администраторами'
+    except FamilyNotInList:
+        error_msg = 'ИИН не найден в списке "Потенциальные получатели АСП с детьми"'
 
     return render_template('index.html', data=iin, family=family.to_dict() if family else None, error=error_msg)
 
