f024b6611a41207f8ec3b969508fe1988b5beaa5
Alikhan Bissakov <78418192+bissakov@users.noreply.github.com>
2023-04-21 00:26:55 +0600

    Changed loading button text

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,7 @@ const submitButton = document.querySelector('button[type="submit"]');
 submitButton.addEventListener('click', () => {
   submitButton.classList.add('loading');
   submitButton.disabled = true;
-  submitButton.textContent = ' Загрузка...';
+  submitButton.textContent = ' Ожидание...';
   spinner = document.createElement('i');
   spinner.classList.add('fa', 'fa-spinner', 'fa-spin');
   submitButton.insertBefore(spinner, submitButton.firstChild);
