2335b96f1217824f925f7926cd173a422872cfa0
bissakov <78418192+bissakov@users.noreply.github.com>
2023-04-23 21:37:54 +0600

    Added api get endpoint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import httpx
-from flask import Flask, request, render_template
+from flask import Flask, json, request, render_template, jsonify
 
 from family.custom_exceptions import FamilyNotFound, WrongPassword, WrongIIN
 from family.family import get_family_data
@@ -8,18 +8,31 @@ from family.family import get_family_data
 app = Flask(__name__)
 
 
+@app.route('/family', methods=['GET'])
+def get_family():
+    iin = request.args.get('iin')
+    try:
+        family_data = get_family_data(iin=iin)
+        return jsonify(({'data': family_data, 'success': True}))
+    except (FamilyNotFound, WrongIIN, WrongPassword) as e:
+        return jsonify({'error_msg': e.error_msg ,'success': False})
+
+
+
 @app.route('/', methods=['GET', 'POST'])
-def home() -> str:
+def index() -> str:
     iin = request.form.get('data', '')
 
+    base_html = 'base.html'
+
     if request.method != 'POST':
-        return render_template('index.html', data=iin, family=None, error=None)
+        return render_template(base_html, data=iin, family=None, error=None)
 
     family = None
     error_msg = None
 
     if iin == '':
-        return render_template('index.html', data=iin, family=family, error='Введите ИИН')
+        return render_template('base.html', data=iin, family=family, error='Введите ИИН')
 
     try:
         family = get_family_data(iin)
@@ -32,7 +45,7 @@ def home() -> str:
     except WrongIIN:
         error_msg = 'Неверный ИИН. ИИН должен состоять из 12 цифр без букв и пробелов'
 
-    return render_template('index.html', data=iin, family=family if family else None, error=error_msg)
+    return render_template('base.html', data=iin, family=family if family else None, error=error_msg)
 
 
 if __name__ == '__main__':
