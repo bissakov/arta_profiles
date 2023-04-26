8c401b319d40e9d1941d2ebee57fbb84510d2d6a
bissakov <78418192+bissakov@users.noreply.github.com>
2023-04-26 21:35:34 +0600

    Update

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,12 @@
-const submitButton = document.querySelector('button[type="submit"]');
+const submitButton = document.querySelector('button');
+const inputBox = document.querySelector('input');
 
 submitButton.addEventListener('click', () => {
   document.querySelector('form').submit()
   submitButton.classList.add('loading');
   submitButton.disabled = true;
   submitButton.textContent = ' Ожидание...';
-  spinner = document.createElement('i');
+  const spinner = document.createElement('i');
   spinner.classList.add('fa', 'fa-spinner', 'fa-spin');
   submitButton.insertBefore(spinner, submitButton.firstChild);
 });
